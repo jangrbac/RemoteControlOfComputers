84fb7977ff998630cc523bc927ec4bc89b5d8904
jangrbac <jan.grbac@gmail.com>
2022-06-04 13:38:42 +0200

    Update set_grub_to_Win_and_reboot.sh
    Cleared unnecessary comment and empty line.

--- set_grub_to_Win_and_reboot.sh
+++ set_grub_to_Win_and_reboot.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 WINDOWS_ENTRY="`grep menuentry -i /boot/grub/grub.cfg | grep Windows | sed -e "s/menuentry '\(.*\)' --class.*/\1/g"`"
-#echo $WINDOWS_ENTRY
 sudo grub-reboot "$WINDOWS_ENTRY"
 sudo reboot
